04932db8cf4b48e3e51d53a22ac005dfa992a8b9
Lexi <[omitted]>
2023-10-17 10:04:52 -0500

    proof of concept for function

--- content_script.js
+++ content_script.js
@@ -1,3 +1,35 @@
 // Put all the javascript code here, that you want to execute after page load.
 
 console.log('Better Playlists Content Script Loaded');
+
+let add_to_playlist = (url) => {
+  console.log(`adding ${url} to playlist`);
+};
+
+let main = () => {
+  let our_input = document.getElementById('search_input');
+
+  our_input.addEventListener('input', (e) => {
+    add_to_playlist(our_input.value);
+    our_input.value = '';
+  });
+};
+
+let create_input = () => {
+  let target = document.querySelector('#end');
+  let input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('id', 'search_input');
+  input.setAttribute(
+    'style',
+    'width: 10px; height: 30px; margin-bottom: 10px;'
+  );
+
+  target.appendChild(input);
+};
+
+document.addEventListener('yt-navigate-finish', (e) => {
+  console.log('youtube finished loading');
+  create_input();
+  main();
+});
